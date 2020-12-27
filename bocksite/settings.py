6e1a549c7ee159d45d5f8ec2622088c4da13de4f
Ciurski <s.bidowaniec@gmail.com>
2020-12-27 22:50:02 +0100

    cors

--- bocksite/settings.py
+++ bocksite/settings.py
@@ -62,12 +62,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'competitors',
     'tournaments',
 ]
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    # others
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,6 +82,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    '127.0.0.1',
+    'http://localhost:3000',
+    'bock-django.herokuapp.com'
+)
 
 ROOT_URLCONF = 'bocksite.urls'
 
